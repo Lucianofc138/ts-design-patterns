fe66f55fab5f2d7421fb1d7be1536a78f53791f8
Lucianofc138 <lucianofc138@gmail.com>
2022-09-25 23:16:22 -0300

    feat: add basic README and first creational example (Factory)

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# ts-design-patterns
-Playground to learn and practice design patterns with typescript
+# Typescript design patterns
+
+Playground to learn and practice design patterns with typescript.
+
+## What's a design pattern
+
+A design pattern is the re-usable form of a solution to a design problem. They are specifically made for OOP and since
+they are part of the solution design, they don't need code to be explained, instead they are usually described through
+some kind of classes diagram, as it could be a UML diagram.
+
+## Types of design patterns
+
+* **Creational**: their main purpose is to create classes or objects, which are the 2 subcategories that usually organize
+this design pattern (Class-creational and object-creational). Examples: [*Factory*](##factory)
+* **Structural**: these patterns organize classes and object in order to achieve bigger structures and get new 
+functionalities.
+* **Behavioral**: these abstract different patterns of communication between objects.
+
+## Creational patterns
+### Factory
+
+This pattern encapsulates the creation of an object inside a class (Factory). The objects can be an instance of 
+different kinds of classes, but these classes always have to follow the same interface (Product).
+
+**Examples**:
+1. **Mario Bros. enemy factory**: this example is based on a [video from BettaTech](https://youtu.be/lLvYAzXO7Ek).
+2. ...
+
+## Structural patterns
+
+## Behavioral patterns
---
+++ src/creational/factory/1-enemy-factory-example.ts
@@ -0,0 +1,109 @@
+interface Enemy {
+  speed: number;
+}
+
+class Boo implements Enemy {
+  speed = 2;
+}
+
+class Koopa implements Enemy {
+  speed = 3;
+  
+}
+
+class Goomba implements Enemy {
+  speed = 2;
+}
+
+interface EnemyFactory {
+  createEnemy(): Enemy;
+}
+
+class BasicEnemyFactory implements EnemyFactory {
+  createEnemy(): Enemy {
+    const r: number = Math.random();
+    return r < 1 / 3 ? new Boo() : r < 2 / 3 ? new Koopa() : new Goomba();
+  }
+}
+
+class SpecificEnemyFactory implements EnemyFactory {
+  constructor(
+    readonly enemiesProbabilities: Array<EnemyGetterWithProbability>
+  ) {}
+
+  createEnemy(): Enemy {
+    const total: number = this.enemiesProbabilities.reduce(
+      (prev: number, curr: EnemyGetterWithProbability) => prev + curr.value,
+      0
+    );
+    const accumulatedProbabilities: Array<EnemyGetterWithProbability> =
+      this.enemiesProbabilities.reduce(
+        (
+          prev: Array<EnemyGetterWithProbability>,
+          curr: EnemyGetterWithProbability
+        ) => {
+          const lastProbability: number = prev.at(-1)?.value || 0;
+          const value: number = curr.value / total + lastProbability;
+          return [...prev, { ...curr, value }];
+        },
+        []
+      );
+    const r: number = Math.random();
+    const enemy: Enemy | null = accumulatedProbabilities.reduce(
+      (prev: Enemy | null, curr: EnemyGetterWithProbability) => {
+        if (!prev && curr.value > r && curr.getInstance) {
+          return curr.getInstance();
+        }
+        return prev;
+      },
+      null
+    );
+    return enemy as Enemy;
+  }
+}
+
+interface EnemyGetterWithProbability {
+  getInstance: () => Enemy;
+  value: number;
+}
+
+if (require.main === module) {
+  const ENEMY_GROUP_SIZE = 20;
+
+  /*********************** Basic enemy factory example ***********************/
+
+  const basicEnemyArray: Enemy[] = [];
+  const basicEnemyFactory: EnemyFactory = new BasicEnemyFactory();
+  for (let i = 0; i < ENEMY_GROUP_SIZE; i++) {
+    const enemy: Enemy = basicEnemyFactory.createEnemy();
+    basicEnemyArray.push(enemy);
+  }
+  console.log("-------------------------------------------------------------");
+  console.log("BASIC ENEMY ARRAY WITH EVEN PROBABILITIES FOR EACH ENEMY TYPE");
+  console.log(basicEnemyArray);
+
+  /********************** Specific enemy factory example *********************/
+
+  const specificEnemyArray: Enemy[] = [];
+  const enemiesProbabilities: EnemyGetterWithProbability[] = [
+    { getInstance: () => new Boo(), value: 1 },
+    { getInstance: () => new Goomba(), value: 2 },
+    { getInstance: () => new Koopa(), value: 2 },
+  ];
+  const specificEnemyFactory: SpecificEnemyFactory = new SpecificEnemyFactory(
+    enemiesProbabilities
+  );
+  for (let i = 0; i < ENEMY_GROUP_SIZE; i++) {
+    const enemy: Enemy = specificEnemyFactory.createEnemy();
+    specificEnemyArray.push(enemy);
+  }
+  console.log("-------------------------------------------------------------");
+  console.log(
+    "SPECIFIC ENEMY ARRAY WITH PROBABILITY PROPORTIONS: ",
+    enemiesProbabilities.map((x) => ({
+      instance: x.getInstance(),
+      value: x.value,
+    }))
+  );
+  console.log(specificEnemyArray);
+}
